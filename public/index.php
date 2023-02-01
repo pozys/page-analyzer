9d5002f49132c89eec4940aa1302509fef9f54e9
Культяпов Константин <pozys@mail.ru>
2023-02-01 21:01:04 +0300

    step 2

--- public/index.php
+++ public/index.php
@@ -3,16 +3,24 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello!");
+$templatePath = 'index.phtml';
 
-    return $response;
+$app->get('/', function (Request $request, Response $response) use ($templatePath) {
+    $params = ['hello' => 'Hello!'];
+
+    return $this->get('renderer')->render($response, $templatePath, $params);
 });
 
 $app->run();
