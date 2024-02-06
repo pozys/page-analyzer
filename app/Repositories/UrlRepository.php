ec6b0bc4273c6fc59c8078df7cb62e2acae9c8d3
Культяпов Константин <pozys@mail.ru>
2024-02-06 16:42:25 +0300

    refactor UrlRepository

--- app/Repositories/UrlRepository.php
+++ app/Repositories/UrlRepository.php
@@ -9,59 +9,55 @@ use PDOStatement;
 
 class UrlRepository
 {
-    private PDOStatement $stmt;
+    private PDOStatement $statement;
 
     public function __construct(private PDO $connection, private string $model = Url::class)
     {
     }
 
-    public function list(): PDOStatement
+    public function listUrls(): PDOStatement
     {
         $sql = "SELECT * FROM {$this->getTableName()} ORDER BY created_at DESC";
 
         return $this->connection->query($sql);
     }
 
-    public function insertUrl(array $url): int
+    public function insertUrl(array $urlData): int
     {
         $sql = "INSERT INTO {$this->getTableName()}(name, created_at) VALUES(:name, :date)";
-        $stmt = $this->connection->prepare($sql);
+        $statement = $this->connection->prepare($sql);
 
-        $urlParsed = parse_url($url['name']);
+        $urlParsed = parse_url($urlData['name']);
         $name = Url::getName($urlParsed);
 
-        $stmt->bindValue(':name', $name);
-        $stmt->bindValue(':date', Carbon::now());
+        $statement->bindValue(':name', $name);
+        $statement->bindValue(':date', Carbon::now());
 
-        $stmt->execute();
+        $statement->execute();
 
         return $this->connection->lastInsertId();
     }
 
-    public function getUrl(int $id): array
+    public function getUrlById(int $id): array
     {
-        return $this->findByField('id', $id)->first();
+        return $this->firstByField('id', $id);
     }
 
-    public function findByField(string $field, mixed $value): self
+    public function firstByField(string $field, mixed $value): ?array
+    {
+        $result = $this->findByField($field, $value)->fetch(PDO::FETCH_ASSOC);
+
+        return $result === false ? null : $result;
+    }
+
+    private function findByField(string $field, mixed $value): PDOStatement
     {
         $sql = "SELECT * FROM {$this->getTableName()} WHERE $field = :{$field}";
-        $this->stmt = $this->connection->prepare($sql);
-        $this->stmt->bindValue(":{$field}", $value);
-        $this->stmt->execute();
+        $this->statement = $this->connection->prepare($sql);
+        $this->statement->bindValue(":{$field}", $value);
+        $this->statement->execute();
 
-        return $this;
-    }
-
-    public function first(): ?array
-    {
-        $result = $this->stmt->fetch(PDO::FETCH_ASSOC);
-
-        if ($result === false) {
-            return null;
-        }
-
-        return $result;
+        return $this->statement;
     }
 
     private function getTableName(): string
